e414e10e881350d3245f79adf66c6901dfa1aae5
negishitakuro <t_negi@hotmail.com>
2020-02-15 12:06:09 +0900

    modify Word class for Room

--- app/src/main/java/com/t_negi/www/roomwordsample/Word.kt
+++ app/src/main/java/com/t_negi/www/roomwordsample/Word.kt
@@ -4,5 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// Roomエンティティ
 @Entity(tableName = "word_table")
 class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
