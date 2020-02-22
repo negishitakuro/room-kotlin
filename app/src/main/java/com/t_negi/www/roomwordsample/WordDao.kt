efac22350dacba52097357e449c07d78a4baa49f
negishitakuro <t_negi@hotmail.com>
2020-02-22 11:14:50 +0900

    modify WordDao class

--- app/src/main/java/com/t_negi/www/roomwordsample/WordDao.kt
+++ app/src/main/java/com/t_negi/www/roomwordsample/WordDao.kt
@@ -3,8 +3,12 @@ package com.t_negi.www.roomwordsample
 import androidx.room.Dao
 import androidx.room.Query
 
+/**
+ *
+ */
 @Dao
 interface WordDao {
     @Query("SELECT * from word_table ORDER BY word ASC")
     fun getAlphabetizedWords() :List<Word>
+
 }
